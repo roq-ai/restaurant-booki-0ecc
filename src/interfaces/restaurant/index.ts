1878a87289745f6ed74d98a01d187e135f24362c
roq-ai <>
2023-07-24 08:09:24 +0000

    Initial commit

---
+++ src/interfaces/restaurant/index.ts
@@ -0,0 +1,34 @@
+import { MenuInterface } from 'interfaces/menu';
+import { ReservationInterface } from 'interfaces/reservation';
+import { TableInterface } from 'interfaces/table';
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface RestaurantInterface {
+  id?: string;
+  description?: string;
+  image?: string;
+  name: string;
+  created_at?: any;
+  updated_at?: any;
+  user_id: string;
+  tenant_id: string;
+  menu?: MenuInterface[];
+  reservation?: ReservationInterface[];
+  table?: TableInterface[];
+  user?: UserInterface;
+  _count?: {
+    menu?: number;
+    reservation?: number;
+    table?: number;
+  };
+}
+
+export interface RestaurantGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  description?: string;
+  image?: string;
+  name?: string;
+  user_id?: string;
+  tenant_id?: string;
+}
